9f3a7e4d2b94e6c35041b747ec187d4bb4ff9505
Murilo Fischer <mufischer@hotmail.com>
2023-11-10 09:42:32 -0300

    criação da home e postdetail

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,40 +1,60 @@
+// CSS
 import styles from "./Home.module.css";
 
 // hooks
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useNavigate, Link } from "react-router-dom";
+
+// react
 import { useState } from "react";
 
 // components
+import PostDetail from "../../components/PostDetail";
 
 const Home = () => {
+  const { documents, loading } = useFetchDocuments("posts");
+
+  const navigate = useNavigate();
+
   const [query, setQuery] = useState("");
-  const [posts] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(query);
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
   };
+
+  // console.log(`loading: ${loading}`);
+
   return (
     <div className={styles.home}>
-      <h1>Veja nossos posts mais recentes!</h1>
-      <form onSubmit={handleSubmit} className={styles.search_form}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="Ou busque por tags"
-          value={query}
+          placeholder="Ou busque por tags..."
           onChange={(e) => setQuery(e.target.value)}
         />
         <button className="btn btn-dark">Pesquisar</button>
       </form>
-      <div>
-        <h1>Posts...</h1>
-        {posts && posts.length === 0 && (
+      <div className="post-list">
+        {loading && <p>Carregando...</p>}
+        {documents && documents.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
             <Link to="/posts/create" className="btn">
-              Criar primeiro Posts
+              Criar primeiro post
             </Link>
           </div>
         )}
+        {console.log(documents.map((post) => `post.title: ${post.title}`))}
+        {console.log(documents.map((post) => `post.id: ${post.id}`))}
+        {documents &&
+          documents.map((post) => <PostDetail key={post.id} post={post} />)}
+        {/* {documents &&
+          documents.map((post) => <h3 key={post.id}>{post.body}</h3>)} */}
       </div>
     </div>
   );
