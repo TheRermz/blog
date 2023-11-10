2d9c83ecafe0d6886bd2e410ca20a702f33e627b
Murilo Fischer <mufischer@hotmail.com>
2023-11-10 17:06:11 -0300

    criado componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Register from "./pages/Register/Register";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -43,6 +44,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/search" element={<Search />} />
+              <Route path="/posts/:id" element={<Post />} />
               <Route
                 path="/login"
                 element={!user ? <Login /> : <Navigate to="/" />}
